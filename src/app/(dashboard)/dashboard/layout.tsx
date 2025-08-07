613ffd02a41e245d837e403d85453770d903734a
apponislam <11appon11@gmail.com>
2025-08-08 02:59:34 +0600

    dashboard home made

--- src/app/(dashboard)/dashboard/layout.tsx
+++ src/app/(dashboard)/dashboard/layout.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuSeparator, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar";
-import { Bell } from "lucide-react";
+import { Bell, Camera, LogOut, SquarePen } from "lucide-react";
 import Image from "next/image";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
@@ -21,19 +22,36 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                     </div>
                     <div className="flex items-center gap-3 md:gap-8  w-full justify-end">
                         <Bell />
-                        <div className="flex items-center gap-3">
-                            <Image src="/people.svg" alt="User Photo" width={58} height={58} className="w-[30px] h-[30px] md:w-[58px] md:h-[58px]" />
-                            <div>
-                                <p className="text-2xl mb-2">John Doe</p>
-                                <h6 className="text-[14px]">Admin</h6>
-                            </div>
-                        </div>
+
+                        <DropdownMenu>
+                            <DropdownMenuTrigger className="border-0 focus:outline-none focus:ring-0">
+                                <div className="flex items-center gap-3 text-left">
+                                    <Image src="/people.svg" alt="User Photo" width={58} height={58} className="w-[30px] h-[30px] md:w-[58px] md:h-[58px]" />
+                                    <div>
+                                        <p className="text-2xl mb-2">John Doe</p>
+                                        <h6 className="text-[14px]">Admin</h6>
+                                    </div>
+                                </div>
+                            </DropdownMenuTrigger>
+                            <DropdownMenuContent className="w-[180px]">
+                                <DropdownMenuItem>
+                                    <Camera /> Change Photo
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <SquarePen className="text-[#D9BD7B]" />
+                                    Change Name
+                                </DropdownMenuItem>
+                                <DropdownMenuSeparator />
+                                <DropdownMenuItem>
+                                    <LogOut className="text-[#E40202]" />
+                                    Logout
+                                </DropdownMenuItem>
+                            </DropdownMenuContent>
+                        </DropdownMenu>
                     </div>
                 </div>
-                <div className="container">
-                    <h1>hiiiii</h1>
-                    {children}
-                </div>
+                <div className="container mx-auto">{children}</div>
             </main>
         </SidebarProvider>
     );
