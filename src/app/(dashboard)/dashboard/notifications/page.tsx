ad8556f05736e45dc25180d0cf56698c9d5a1e7e
Appon Islam <appon2003@gmail.com>
2025-08-13 03:51:47 +0600

    notification page error fixed

--- src/app/(dashboard)/dashboard/notifications/page.tsx
+++ src/app/(dashboard)/dashboard/notifications/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 
 const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 
-export async function fetchNotifications(): Promise<Notification[] | null> {
+async function fetchNotifications(): Promise<Notification[] | null> {
     try {
         const res = await fetch(`${baseUrl}/notifications/notifications.json`, {
             cache: "no-store",
@@ -19,8 +19,6 @@ export async function fetchNotifications(): Promise<Notification[] | null> {
         if (!res.ok) return null;
 
         const json: ApiResponse<Notification[]> = await res.json();
-
-        // Works for both "status" and "success"
         const statusValue = json.status || json.success;
 
         if (statusValue === "success") {
@@ -36,8 +34,6 @@ export async function fetchNotifications(): Promise<Notification[] | null> {
 
 const page = async () => {
     const notifications = await fetchNotifications();
-
-    console.log(notifications);
 
     if (!notifications) {
         return <div>Failed to load notifications</div>;
